12ec6aea6f5baa171ad13978f72e78f282cef928
Ali Saviz <ali.saviz@gmail.com>
2020-12-09 23:03:36 +0100

    making migration of campus, areas and programs

---
+++ database/factories/ProgramAreaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ProgramArea;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProgramAreaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ProgramArea::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2020_12_09_215310_create_campuses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('street');
+            $table->integer('house_nr');
+            $table->string('house_nr_addition');
+            $table->string('zip_code');
+            $table->string('city');
+            $table->string('email');
+            $table->string('phone_nr');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campuses');
+    }
+}
