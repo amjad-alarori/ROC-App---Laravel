a4dcf205ba15015735892912bd95fdc505bfc65b
Ali Saviz <ali.saviz@gmail.com>
2021-01-09 22:14:40 +0100

    Working aan de controller

--- app/Http/Controllers/QFileController.php
+++ app/Http/Controllers/QFileController.php
@@ -13,16 +13,15 @@ class QFileController extends Controller
         $grades = $user->grades->whereIn('course_plan_id', $plans->pluck('id')->toArray())->load('coursePlan.subject.attachedCompetences');
 
 
-
-
         //not works. test is still empty. use foreach loop
-        $test = [];
-        $heh = $plans->map(function ($plan) use($test){
-           return $plan->subject->attachedCompetences->map(function ($comp) use ($test){
-               return $test[$comp->id] = $comp;
-           });
-        });
+        $courseComps = [];
+        foreach ($plans as $plan):
+            foreach ($plan->subject->attachedCompetences as $competence):
+                $courseComps[$competence->id] = $competence;
+            endforeach;
+        endforeach;
 
-        dd($plans, $grades, $test, $heh);
+
+        dd($plans, $grades, $courseComps);
     }
 }
