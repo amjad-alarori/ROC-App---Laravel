9c78c0700f8806c7ea97388522f0923067d25c95
Ali Saviz <ali.saviz@gmail.com>
2021-01-10 04:08:20 +0100

    rebase master and prepair the QFileController for Roderiks blade

--- app/Http/Controllers/QFileController.php
+++ app/Http/Controllers/QFileController.php
@@ -9,19 +9,31 @@ class QFileController extends Controller
 {
     public function __invoke(User $user, Course $course)
     {
-        $plans = $course->plans->load('subject.attachedCompetences');
+        $plans = $course->plans()->with('subject.attachedCompetences')->whereHas('subject', function ($q) {
+            $q->where('co_op', '=', false);
+        })->get();
+
         $grades = $user->grades->whereIn('course_plan_id', $plans->pluck('id')->toArray())->load('coursePlan.subject.attachedCompetences');
 
+        $passedComp = [];
+        foreach ($grades as $grade):
+            foreach ($grade->coursePlan->subject->attachedCompetences as $comp):
+                $passedComp[$comp->id] = $comp;
+            endforeach;
+        endforeach;
 
-        //not works. test is still empty. use foreach loop
         $courseComps = [];
         foreach ($plans as $plan):
             foreach ($plan->subject->attachedCompetences as $competence):
-                $courseComps[$competence->id] = $competence;
+                if (!array_key_exists($competence->id, $passedComp)):
+                    $courseComps[$competence->id] = $competence;
+                endif;
             endforeach;
         endforeach;
 
 
-        dd($plans, $grades, $courseComps);
+        dd($plans, $grades, $passedComp, $courseComps);
+
+        return view('qualificationFile');
     }
 }
